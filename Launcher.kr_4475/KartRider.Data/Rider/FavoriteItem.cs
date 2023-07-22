ea9ee43d8a1e319b27873bc32cd7cb1eb43501e3
LAON <zxfde7732@naver.com>
2023-07-23 02:35:13 +0900

    first commit

---
+++ Launcher.kr_4475/KartRider.Data/Rider/FavoriteItem.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KartRider.IO;
+using KartRider;
+
+namespace RiderData
+{
+    public static class FavoriteItem
+    {
+        public static void Favorite_Item()
+        {
+            int itemCount = 17;
+            using (OutPacket outPacket = new OutPacket("PrFavoriteItemGet"))
+            {
+                if (Program.FavoriteItem)
+                {
+                    outPacket.WriteInt(itemCount);
+
+                    outPacket.WriteShort(3);
+                    outPacket.WriteShort(839);
+                    outPacket.WriteShort(301);
+                    outPacket.WriteByte(0);
+
+                    outPacket.WriteShort(3);
+                    outPacket.WriteShort(854);
+                    outPacket.WriteShort(301);
+                    outPacket.WriteByte(0);
+
+                    outPacket.WriteShort(3);
+                    outPacket.WriteShort(937);
+                    outPacket.WriteShort(301);
+                    outPacket.WriteByte(0);
+
+                    outPacket.WriteShort(3);
+                    outPacket.WriteShort(955);
+                    outPacket.WriteShort(301);
+                    outPacket.WriteByte(0);
+
+                    outPacket.WriteShort(3);
+                    outPacket.WriteShort(1017);
+                    outPacket.WriteShort(301);
+                    outPacket.WriteByte(0);
+
+                    outPacket.WriteShort(3);
+                    outPacket.WriteShort(1031);
+                    outPacket.WriteShort(301);
+                    outPacket.WriteByte(0);
+
+                    outPacket.WriteShort(3);
+                    outPacket.WriteShort(1034);
+                    outPacket.WriteShort(301);
+                    outPacket.WriteByte(0);
+
+                    outPacket.WriteShort(3);
+                    outPacket.WriteShort(1044);
+                    outPacket.WriteShort(301);
+                    outPacket.WriteByte(0);
+
+                    outPacket.WriteShort(3);
+                    outPacket.WriteShort(1045);
+                    outPacket.WriteShort(301);
+                    outPacket.WriteByte(0);
+
+                    outPacket.WriteShort(3);
+                    outPacket.WriteShort(1098);
+                    outPacket.WriteShort(301);
+                    outPacket.WriteByte(0);
+
+                    outPacket.WriteShort(3);
+                    outPacket.WriteShort(1126);
+                    outPacket.WriteShort(14);
+                    outPacket.WriteByte(0);
+
+                    outPacket.WriteShort(3);
+                    outPacket.WriteShort(1133);
+                    outPacket.WriteShort(18);
+                    outPacket.WriteByte(0);
+
+                    outPacket.WriteShort(3);
+                    outPacket.WriteShort(1140);
+                    outPacket.WriteShort(21);
+                    outPacket.WriteByte(0);
+
+                    outPacket.WriteShort(3);
+                    outPacket.WriteShort(1160);
+                    outPacket.WriteShort(26);
+                    outPacket.WriteByte(0);
+
+                    outPacket.WriteShort(3);
+                    outPacket.WriteShort(1168);
+                    outPacket.WriteShort(29);
+                    outPacket.WriteByte(0);
+
+                    outPacket.WriteShort(3);
+                    outPacket.WriteShort(1170);
+                    outPacket.WriteShort(31);
+                    outPacket.WriteByte(0);
+
+                    outPacket.WriteShort(3);
+                    outPacket.WriteShort(1176);
+                    outPacket.WriteShort(36);
+                    outPacket.WriteByte(0);
+                }
+                else
+                {
+                    outPacket.WriteInt(0);
+                }
+                RouterListener.MySession.Client.Send(outPacket);
+            }
+        }
+    }
+}
